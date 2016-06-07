4046d2a3ac8b09cda691d8f5cb97940f04b377bc
John Smidt <johnmsmidt@gmail.com>
2016-06-07 07:11:02 -0600

    more pushing

--- Project/dbConnect.php
+++ Project/dbConnect.php
@@ -13,25 +13,25 @@ function connectToDb()
      $dbName = "testdb";
 
      $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
-     // echo $openShiftVar;
+      echo $openShiftVar;
 
      if ($openShiftVar === null || $openShiftVar == "")
      {
           // Not in the openshift environment
-          //echo "Using local credentials: ";
+          echo "Using local credentials: ";
           //require("setLocalDatabaseCredentials.php");
      }
      else
      {
           // In the openshift environment
-          //echo "Using openshift credentials: ";
+          echo "Using openshift credentials: ";
 
           $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
           $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
           $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
           $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
      }
-     //echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+     echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
 
      $db = new PDO("mysql:host=localhost;dbname=construct", $dbUser, $dbPassword);
 
